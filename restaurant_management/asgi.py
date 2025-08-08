4cda8462c7c26aaf8e573a39aa2bc6cc6fbc0618
anshulgoel20 <anshul.goel7100@gmail.com>
2025-08-08 11:59:02 +0530

    26709

--- restaurant_management/asgi.py
+++ restaurant_management/asgi.py
@@ -14,3 +14,11 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'restaurant_management.settings')
 
 application = get_asgi_application()
+body,h1,h2,h3,h4
+margin:0;
+padding:0;
+}
+body{
+    font-family:arial,sans-serif;
+    
+}
