5c594364d1d3507bb0f61c61dc7e8fe0c75dd109
anshulgoel20 <anshul.goel7100@gmail.com>
2025-08-21 11:48:17 +0530

    36891

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -127,9 +127,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-restaurant_management=settings.
-
-restaurant_management
+restaurant_management=settings.restaurant_management
 phone_number=settings.restaurant_management
 return render(request,"home.html")
 
@@ -149,3 +147,4 @@ main{
 <UserAttributeSimilarityValidator
 <MinimumLengthValidator
 import user from pathlib
+display the restaurant_management name
